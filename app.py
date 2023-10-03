902f926d7599f06ba3c39d201a13d21e3d58f4a1
CharlesJub <charlie.jubera@gmail.com>
2023-10-03 00:10:12 -0400

    remove sqlite call

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import psycopg2
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db_settings = {
     'dbname': creds.DB_NAME,
